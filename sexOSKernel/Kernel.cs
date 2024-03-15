44eb29423f0499ddefaae6f90410a255f151c404
danDrilea <danalexandrudrilea@gmail.com>
2024-03-15 18:23:08 +0200

    dan GUI attempt (ignore)

--- sexOSKernel/Kernel.cs
+++ sexOSKernel/Kernel.cs
@@ -6,6 +6,8 @@ using System.Text;
 using Sys = Cosmos.System;
 using sexOSKernel.Commands;
 using Cosmos.System.FileSystem;
+using sexOSKernel.Graphics;
+
 namespace sexOSKernel//<------ INCEPUT SCOPE KERNEL
 {
     ///nu IESITI DIN NAMESPACE CA FACE ASTA URAT
@@ -14,12 +16,16 @@ namespace sexOSKernel//<------ INCEPUT SCOPE KERNEL
     {
         public CosmosVFS vfs;//file system class
         private CommandManager commandManager;
+
+        public static GUI gui;
+
         //imi bag pula in ea de viata
         protected override void BeforeRun()
         {
             this.vfs = new CosmosVFS();//register vfs
             Sys.FileSystem.VFS.VFSManager.RegisterVFS(this.vfs);
             this.commandManager = new CommandManager();//adauga comenzile ca sa fie recunoscute in scope
+
             Console.Write("                ___  ____  \r\n ___  _____  __/ _ \\/ ___| \r\n/ __|/ _ \\ \\/ / | | \\___ \\ \r\n\\__ \\  __/>  <| |_| |___) |\r\n|___/\\___/_/\\_\\\\___/|____/ \n");
             Console.Write("sexOS successfully booted!\n");
         }
@@ -27,6 +33,14 @@ namespace sexOSKernel//<------ INCEPUT SCOPE KERNEL
         ///Intro song functions
         protected override void Run()
         {
+            //if gui open
+            if(Kernel.gui != null)
+            {
+                Kernel.gui.handleGUIInputs();
+                return;
+            }
+            //else
+
             String input = Console.ReadLine();
             string response = this.commandManager.processInput(input);
             Console.WriteLine(response);
